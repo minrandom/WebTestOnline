7ef0e7ad367bbce8cd2f52ddde6c5ea07e8d9a2c
Jojo Pattinama <pattinamajojo@gmail.com>
2025-06-16 16:56:14 +0700

    starter ajadeh

---
+++ app/Http/Controllers/LearningController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LearningController extends Controller
+{
+    //
+    public function index(){
+        return "helo murid, semangat belajar";
+    }
+
+
+}
